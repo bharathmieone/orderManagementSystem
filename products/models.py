641a25309ff108ab35ba18d4d7b77bcc77eed02a
bharath <bharathjinka09@gmail.com>
2019-10-02 14:18:45 +0530

    added admin titles

--- products/models.py
+++ products/models.py
@@ -8,10 +8,17 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
     sold  = models.IntegerField()
-    stock = models.IntegerField()  
+    stock = models.IntegerField() 
+
+    def __str__(self):
+        return self.title
+
 
 class Alerts(models.Model):
 
     alert = models.TextField(max_length=250)
     date = models.DateTimeField(default=datetime.now, blank=True)
     offer = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.alert
